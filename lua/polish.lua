18181797496d94d0057670636a8da646dbd19a74
Tux Node <tuxnode@outlook.com>
2026-04-29 21:03:32 +0800

    Add: Disable relativenumber

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,5 +1,7 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
+vim.opt.relativenumber = false -- 关闭相对行号
+vim.opt.number = true          -- 确保绝对行号开启
