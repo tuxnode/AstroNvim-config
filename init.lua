32530eda4a8af52018137a2dac9f17ffeef4351c
tuxnode <tuxnode@outlook.com>
2026-06-30 19:51:55 +0800

    fix: 剪贴板互通

--- init.lua
+++ init.lua
@@ -14,6 +14,7 @@ if not (vim.env.LAZY or (vim.uv or vim.loop).fs_stat(lazypath)) then
 end
 
 vim.opt.rtp:prepend(lazypath)
+vim.o.clipboard = "unnamedplus"
 
 -- validate that lazy is available
 if not pcall(require, "lazy") then
