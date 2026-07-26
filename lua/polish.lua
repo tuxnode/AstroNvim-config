641ac19e4724be73b2bab55632d63f0db6175610
tuxnode <tuxnode@outlook.com>
2026-07-26 20:55:28 +0800

    fix: set cpp default tab is 2

--- lua/polish.lua
+++ lua/polish.lua
@@ -7,7 +7,7 @@ vim.opt.relativenumber = false -- 关闭相对行号
 vim.opt.number = true          -- 确保绝对行号开启
 
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "c", "cpp" },
+  pattern = { "c" },
   callback = function()
     vim.opt_local.tabstop = 4
     vim.opt_local.shiftwidth = 4
