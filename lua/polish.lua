47e3356a0b7982ec0d79d11fa2c39ceb3aac26d9
tuxnode <tuxnode@outlook.com>
2026-08-05 15:25:42 +0800

    fix: delete C 4 indentation

--- lua/polish.lua
+++ lua/polish.lua
@@ -5,13 +5,3 @@
 -- fit in the normal config locations above can go here
 vim.opt.relativenumber = false -- 关闭相对行号
 vim.opt.number = true          -- 确保绝对行号开启
-
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "c" },
-  callback = function()
-    vim.opt_local.tabstop = 4
-    vim.opt_local.shiftwidth = 4
-    vim.opt_local.softtabstop = 4
-    vim.opt_local.expandtab = true
-  end,
-})
